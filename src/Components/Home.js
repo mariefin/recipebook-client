431e869ff57ed90f2022af9f47463ff9f0d4b905
mariefin <maikki_maa_93@hotmail.com>
2021-04-23 22:39:05 +0300

    Testing

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../index.css';
-const apiUrl = `http://localhost:8080`;
+// const apiUrl = `http://localhost:8080`;
+const apiUrl = `http://${window.location.hostname}:8080`;
 // const apiUrl = `http://recipeserver:8080`;
 
 class Home extends Component {
